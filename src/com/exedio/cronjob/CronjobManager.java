b85b145da43bc7d21edbfc70d0aad3c9cbdc06fa
Peter Hentschel <peter.hentschel@exedio.com>
2006-05-22 12:56:25 +0000

    small changes
    git-svn-id: https://svn.exedio.com/tools/trunk/cronjob@1397 8ceed453-710c-0410-87a1-99b3265506e7

--- src/com/exedio/cronjob/CronjobManager.java
+++ src/com/exedio/cronjob/CronjobManager.java
@@ -241,7 +241,7 @@ public class CronjobManager extends HttpServlet
 				"<tr><td align=left>"+
 				"To install a new cronjob, just follow the instuctions below:<br>"+
 				"&nbsp;&nbsp;&nbsp;1. The cronjob-class has to implement the Chronjob-interface<br>"+
-				"&nbsp;&nbsp;&nbsp;2. The cronjob-class must be added to the method getAllCronjobs() in the class: "+storeName +"<br>"+
+				"&nbsp;&nbsp;&nbsp;2. The cronjob-class must be added to the method getAllCronjobs() in the class:<b> "+storeName +"</b><br>"+
 				"that's all !!!"+
 				"</td></tr></table>";
 		}
