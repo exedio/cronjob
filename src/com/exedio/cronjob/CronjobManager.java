8222a12e7b02bb9f765ad01871c30ced3d477a8a
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2008-10-10 19:26:51 +0000

    use jspm
    git-svn-id: https://svn.exedio.com/tools/trunk/cronjob@2285 8ceed453-710c-0410-87a1-99b3265506e7

--- src/com/exedio/cronjob/CronjobManager.java
+++ src/com/exedio/cronjob/CronjobManager.java
@@ -24,7 +24,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletConfig;
@@ -155,18 +154,19 @@ public class CronjobManager extends HttpServlet
 		doRequest(request, response);
 	}
 	
+	static final String ACTIVATE="activate";
+	static final String DEACTIVATE="deactivate";
+	static final String TRUE = "true";
+	static final String FALSE = "false";
+	static final String AUTO_REFRESH = "autoRefresh";
+	static final String START_CRONJOB = "Start";
+	static final String DELETE_LAST_EXCEPTION = "Delete";
+	static final String ENABLE_AUTOREFRESH = "Enable Auto-Refresh";
+	static final String DISABLE_AUTOREFRESH = "Disable Auto-Refresh";
+	
 	private void doRequest(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException
 	{
 		String uri=request.getRequestURI();
-		final String ACTIVATE="activate";
-		final String DEACTIVATE="deactivate";
-		final String TRUE = "true";
-		final String FALSE = "false";
-		final String AUTO_REFRESH = "autoRefresh";
-		final String START_CRONJOB = "Start";
-		final String DELETE_LAST_EXCEPTION = "Delete";
-		final String ENABLE_AUTOREFRESH = "Enable Auto-Refresh";
-		final String DISABLE_AUTOREFRESH = "Disable Auto-Refresh";
 		// Start/Activate/Deactivate/removeLastException for selected Cronjob
 		
 		for (final ObservedCronjob job : observedCronjobs)
@@ -209,106 +209,16 @@ public class CronjobManager extends HttpServlet
 			autoRefreshPage=false;
 			enableOrDisableAutoRefreshButton=ENABLE_AUTOREFRESH;
 		}
-		
-		// Page-Content-Generation
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
-		String result="<html>\n<head>\n<title>Cronjob Manager</title>";
-		uri=uri+"?"+AUTO_REFRESH+"="+(autoRefreshPage ? TRUE : FALSE);
-		if (autoRefreshPage)
-		{			
-			result+="\n<meta http-equiv=\"refresh\" content=\"5; URL="+uri+"\">";
-		}
-		result+="\n<style type=\"text/css\">\n th, td \n{ \npadding: 0 5 0 5\n}\n</style>";
-		result+="</head>\n<body><form action=\""+uri+"\" method=\"post\">\n<table width=100%><tr><td align=center>\n<h1>exedio cronjob</h1>";			
-		if (!observedCronjobs.isEmpty())
-		{	
-			result+="<table><tr><td><input type=submit name=\""+enableOrDisableAutoRefreshButton+"\" value=\""+enableOrDisableAutoRefreshButton+"\"/>" +
-				"</td>" +
-				"</tr></table><br>"+
-				"<table border=1 width=1% cellpadding=0 cellspacing=0>"+
-				"<tr>" +
-				"<th rowspan=2>&nbsp;</th>" +
-				"<th rowspan=2>&nbsp;</th>" +
-				"<th rowspan=2>Name</th>" +
-				"<th rowspan=2><nobr>Intervall (m)</nobr></th>" +
-				"<th rowspan=2>Running</th>" +
-				"<th colspan=3>Last Execution</th>" +			
-				"<th rowspan=2>Successful Executions</th>" +
-				"<th rowspan=2>Fails</th>" +
-				"<th rowspan=2>Average Duration</th>" +
-				"</tr>"+
-				"<tr>"+
-				"<th>Status</th>" +		
-				"<th>Start</th>" +		
-				"<th>Duration</th>" +		
-				"</tr>";
-			for (final ObservedCronjob job : observedCronjobs)
-			{
-				String activate = "<input type=submit name=\""+job.getId()+"\" value=\""+ACTIVATE+"\" "+(job.isActivated() ? "disabled readonly" : "")+"/>";
-				String deactivate = "<input type=submit name=\""+job.getId()+"\" value=\""+DEACTIVATE+"\" "+(job.isActivated() ? "" : "disabled readonly")+"/>";
-
-				result+="<tr>"+
-					"<td align=center><input type=submit name=\""+job.getId()+"\" value=\""+START_CRONJOB+"\"/></td>"+
-					"<td align=center><nobr>"+activate+" "+deactivate+"</nobr></td>"+
-					"<td align=center>"+job.getDisplayedName()+"</td>"+
-					"<td align=center>"+String.valueOf(job.getMinutesBetweenExecutions())+"</td>"+
-					"<td align=center>"+(job.isRunning() ? "since "+(new Date().getTime()-job.getLastTimeStarted().getTime())+" ms" : "no")+"</td>"+
-					"<td align=center style=\"font-weight:bold; color:"+((job.wasLastExecutionSuccessful()) ? "green" : "red")+"\">"+((job.wasLastExecutionSuccessful()) ? "OK" : "FAILED")+"</td>"+
-					"<td align=center><nobr>"+job.getLastTimeStartedAsString()+"</nobr></td>"+
-					"<td align=center><nobr>"+job.getTimeNeeded()+"</nobr></td>"+
-					"<td align=center><nobr>"+job.getSuccessfulRuns()+"</nobr></td>"+
-					"<td align=center><nobr>"+job.getNumberOfFails()+"</nobr></td>"+
-					"<td align=center><nobr>"+job.getAverageTimeNeeded()+"</nobr></td>"+
-					"</tr>";;
-			}
-			result+="</table><br>";
-			for (final ObservedCronjob job : observedCronjobs)
-			{
-				List<StackTraceElement> elements= new ArrayList<StackTraceElement>();
-				if (job.getLastException()!=null)
-				{
-					result+="<br><table width=100% cellpadding=0 cellspacing=0>";
-					result+="<tr><td><h3>Stacktrace of last exception occured on Cronjob: "+job.getDisplayedName()+"&nbsp;&nbsp;" +
-						"<input type=submit name=\""+job.getId()+"\" value=\""+DELETE_LAST_EXCEPTION+"\"/></h3></td></tr>";
-					result+="<tr><td>"+job.getLastException().getMessage()+"</td></tr>";
-					elements = Arrays.asList(job.getLastException().getStackTrace());
-					for (final StackTraceElement el : elements)
-					{
-						result+="<tr><td align=left>"+el.toString()+"</td></tr>";
-					}
-					result+="</table>";
-				}
-			}
-		}
-		else
-		{
-			result+="<table width=100%>";
-			if (active)
-			{
-				result+="<tr><td align=center><b>There are currently no cronjobs installed.</b></td></tr>"+
-					"<tr><td>&nbsp;</td></tr>"+
-					"<tr><td align=left>"+
-					"To install a new cronjob, just follow the instuctions below:<br><br>"+
-					"&nbsp;&nbsp;&nbsp;1. The cronjob-class has to implement the Cronjob-interface<br>"+
-					"&nbsp;&nbsp;&nbsp;2. An instance of the cronjob-class must be added to the method getJobs() in the class:<b> "+storeName +"</b><br>"+
-					"</td></tr>";
-			}
-			else
-			{
-				result+="<tr><td align=center><b>The cronjobs are not activated.</b></td></tr>"+
-					"<tr><td>&nbsp;</td></tr>"+
-					"<tr><td align=left>"+
-					"To activate the cronjobs, the class:<b> "+storeName +"</b> has to return true in its isActive() method"+
-					"</td></tr>";
-			}
-			result+="</table>";
-		}
-		result+="<br><table width=100%><tr><td align=right style=\"font-size:12 \"><hr width=100%>"+
-			"exedio cronjob - "+getImplementationVersion()+" - &copy;  <a href=\"http://www.exedio.com/\">exedio</a>" +
-			" - Gesellschaft f&uuml;r Softwareentwicklung mbH (" + System.identityHashCode(this) + ")</td></tr></table>";
-		result+="</td></tr></table></form></body></html>";
-		out.println(result);
+		Page_Jspm.write(out,
+				uri+"?"+AUTO_REFRESH+"="+(autoRefreshPage ? TRUE : FALSE),
+				autoRefreshPage,
+				enableOrDisableAutoRefreshButton,
+				observedCronjobs,
+				getImplementationVersion(),
+				System.identityHashCode(this),
+				active);
 		out.close();
 	}
 }
